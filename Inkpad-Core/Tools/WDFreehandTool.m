2a0b6cc8ef2bfced0e28229497d580bd290e4725
Steve Sprang <scs@stevesprang.com>
2014-01-02 20:37:42 -0800

    Add a new tool to create closed freehand shapes. We might want to process the points a bit differently before smoothing, but this seems ok for a first pass.

--- Inkpad-Core/Tools/WDFreehandTool.m
+++ Inkpad-Core/Tools/WDFreehandTool.m
@@ -19,19 +19,35 @@
 #import "WDInspectableProperties.h"
 #import "WDPath.h"
 #import "WDPropertyManager.h"
+#import "WDUtilities.h"
 
 #define kMaxError 10.0f
 
+NSString *WDDefaultFreehandTool = @"WDDefaultFreehandTool";
+
 @implementation WDFreehandTool
+
+@synthesize closeShape = closeShape_;
 
 - (NSString *) iconName
 {
-    return @"brush.png";
+    return closeShape_ ? @"freehand_shape.png" : @"brush.png";
 }
 
 - (BOOL) createsObject
 {
     return YES;
+}
+
+- (BOOL) isDefaultForKind
+{
+    NSNumber *defaultFreehand = [[NSUserDefaults standardUserDefaults] valueForKey:WDDefaultFreehandTool];
+    return (closeShape_ == [defaultFreehand intValue]) ? YES : NO;
+}
+
+- (void) activated
+{
+    [[NSUserDefaults standardUserDefaults] setValue:@(closeShape_) forKey:WDDefaultFreehandTool];
 }
 
 - (void) beginWithEvent:(WDEvent *)theEvent inCanvas:(WDCanvas *)canvas
@@ -55,6 +71,8 @@
 - (void) endWithEvent:(WDEvent *)theEvent inCanvas:(WDCanvas *)canvas
 {
     if (pathStarted_ && [tempPath_.nodes count] > 1) {
+        float maxError = (kMaxError / canvas.viewScale);
+        
         canvas.shapeUnderConstruction = nil;
         
         NSMutableArray *points = [NSMutableArray array];
@@ -62,7 +80,20 @@
             [points addObject:[NSValue valueWithCGPoint:node.anchorPoint]];
         }
         
-        WDPath *smoothPath = [WDCurveFit smoothPathForPoints:points error:(kMaxError / canvas.viewScale) attemptToClose:YES];
+        if (closeShape_ && tempPath_.nodes.count > 2) {
+            // we're drawing free form closed shapes... let's relax the error
+            maxError *= 10;
+            
+            // add the first point at the end to make sure we close
+            CGPoint first = [points[0] CGPointValue];
+            CGPoint last = [[points lastObject] CGPointValue];
+                
+            if (WDDistance(first, last) > (maxError*2)) {
+                [points addObject:points[0]];
+            }
+        }
+        
+        WDPath *smoothPath = [WDCurveFit smoothPathForPoints:points error:maxError attemptToClose:YES];
         
         if (smoothPath) {
             smoothPath.fill = [canvas.drawingController.propertyManager activeFillStyle];
