578b36f23a81c62a835d994eccb9e5bb72b0b2e6
32Beat <32beat@MBP13.local>
2014-02-04 08:53:33 +0100

    SVG opacity parsing change Introduce check for actual entry since [nil floatValue] will return 0.0 but default opacity is 1.0

--- Inkpad-Core/SVG/WDSVGParser.m
+++ Inkpad-Core/SVG/WDSVGParser.m
@@ -692,7 +692,8 @@
             // only the <svg> group and implicit top-level group are below this one
             WDLayer *layer = [[WDLayer alloc] initWithElements:elements];
             layer.name = layerName ?: xmlid;
-            layer.opacity = [[state_ style:kWDPropertyOpacity] floatValue];
+			if ([state_ style:kWDPropertyOpacity])
+            { layer.opacity = [[state_ style:kWDPropertyOpacity] floatValue]; }
             if ([[state_ style:kWDPropertyVisibility] isEqualToString:@"hidden"] || [[state_ style:kWDPropertyDisplay] isEqualToString:@"none"]) {
                 layer.hidden = YES;
             }
@@ -700,7 +701,8 @@
             [drawing_ addLayer:layer];
         } else if ([elements count] == 1) {
             state_.wdElement = [self clipAndGroup:[elements lastObject]];
-            state_.wdElement.opacity *= [[state_ style:kWDPropertyOpacity] floatValue];
+			if ([state_ style:kWDPropertyOpacity])
+            { state_.wdElement.opacity *= [[state_ style:kWDPropertyOpacity] floatValue]; }
         } else {
             WDGroup *group = [[WDGroup alloc] init];
             group.layer = drawing_.activeLayer;
